66c11655fb8d9a2fe2bf0ca2fefee854a9a58ca9
Janeth Eni <enijaneth@gmail.com>
2023-04-28 05:46:41 +0000

    Annotation && log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
 """ PII Module """
 
+import logging
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ Returns log message obfuscated """
     pattern = fr'({"|".join(fields)})=[^{separator}]+'
     return re.sub(pattern, f'\\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Init function """
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formatter; filters values in incoming log records using filter_datum
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
