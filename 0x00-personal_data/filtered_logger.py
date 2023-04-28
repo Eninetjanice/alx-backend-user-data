f538912a41e2f3a8a3c540c2f349a0759abdd8f0
Janeth Eni <enijaneth@gmail.com>
2023-04-28 05:01:08 +0000

    Filter date

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" PII Module """
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """ Returns log message obfuscated """
+    pattern = fr'({separator}|^)({separator.join(fields)}){separator}'
+    return re.sub(pattern, f'\\1{redaction}{separator}', message)
