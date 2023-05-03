bb0b7af324892ea9762fdc28291eac9fed045609
Janeth Eni <enijaneth@gmail.com>
2023-05-03 15:04:04 +0100

    Variable auth instance fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,12 +15,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv("AUTH_TYPE")
-basic_auth = getenv('AUTH_TYPE')
+#  basic_auth = getenv('AUTH_TYPE')
 
 if auth_type == "basic_auth":
     auth = BasicAuth()
-#  else:
-    #  auth = Auth()
+else:
+    auth = Auth()
 
 excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                   '/api/v1/forbidden/']
